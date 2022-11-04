ea4a1f0bc4f42b5c929e9772502627e0aeb205fa
Francisco <113522254+WillianXG@users.noreply.github.com>
2022-11-04 10:53:58 -0300

    Atualização setTimeOut

--- js/emailrec.js
+++ js/emailrec.js
@@ -26,7 +26,10 @@ function checkInputsRec(){
     });
 
     if (formRecIsValid){
-        console.log("certo")
+        console.log("certo"),
+         setTimeout(()=>{
+            document.location.reload(true)
+        }, "2500")
         
     }
     else{
